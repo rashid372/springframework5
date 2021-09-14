43d4e04a0d6748f8bf84c43cb741707cd67f4a86
rashid <rashid.mumtaz372@gmail.com>
2021-09-14 21:32:09 +0500

    initail commit

---
+++ src/main/java/com/bigdata/Springframework5Application.java
@@ -0,0 +1,13 @@
+package com.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springframework5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springframework5Application.class, args);
+	}
+
+}
