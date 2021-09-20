2f96a6dfb80cc20e85594b73bb32f0dd2c680d33
rashid <rashid.mumtaz372@gmail.com>
2021-09-20 10:48:49 +0500

    refactor to MultiModule Project - Closes #1.

--- springframework5-web/src/main/java/bigdata/Springframework5Application.java
+++ springframework5-web/src/main/java/bigdata/Springframework5Application.java
@@ -1,4 +1,4 @@
-package com.bigdata;
+package bigdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- springframework5-web/src/test/java/bigdata/Springframework5ApplicationTests.java
+++ springframework5-web/src/test/java/bigdata/Springframework5ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bigdata;
+package bigdata;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
